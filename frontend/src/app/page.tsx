972475f2fe16dda4199020b4853880f314ddbba7
DotcomPeep <steamcs2016@gmail.com>
2023-10-19 11:24:04 -0300

    css change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,10 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 // Adapters
-import { createTask, getTasks } from '@/adapters/task'
+import { createTask, deleteTaskById, getTasks } from '@/adapters/task'
+
+// Icons
+import { Trash } from 'phosphor-react'
 
 // Types
 import { TaskDTO } from '@/types/TaskDTO'
@@ -30,78 +33,106 @@ export default function Home() {
     resolver: zodResolver(formDataSchema),
   })
 
+  async function getAllTasks() {
+    try {
+      setIsLoading(true)
+      const data = await getTasks()
+      setTasks(data)
+    } catch (error) {
+      console.error(error)
+      throw error
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   async function handleCreateTask(dataForm: FormDataProps) {
     const data = await createTask(dataForm.title, dataForm.description)
-    setTasks(data)
+    getAllTasks()
+  }
+
+  async function handleDeleteTask(id: number) {
+    await deleteTaskById(id)
+    getAllTasks()
   }
 
   useEffect(() => {
-    const getAllTasks = async () => {
-      try {
-        setIsLoading(true)
-        const data = await getTasks()
-        setTasks(data)
-      } catch (error) {
-        console.error(error)
-        throw error
-      } finally {
-        setIsLoading(false)
-      }
-    }
     getAllTasks()
-  }, [tasks])
+  }, [])
 
   return (
-    <div className="flex justify-center">
-      <h1>Todo List</h1>
-
-      <button onClick={() => setIsVisible(true)}>
+    <div className="flex flex-col items-center p-4">
+      <h1 className="text-2xl font-semibold mb-4">Todo List</h1>
+  
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        onClick={() => setIsVisible(true)}
+      >
         Clique aqui para criar sua tarefa
       </button>
-
+  
       {isVisible && (
-        <div className="flex justify-between">
-          <div className="gap-x-2">
+        <div className="flex justify-between mt-4">
+          <div className="space-x-2">
             <input
+              className="border rounded p-2"
               placeholder="Digite o título da sua tarefa"
               {...register('title')}
             />
-
+  
             <input
+              className="border rounded p-2"
               placeholder="Digite a descrição da sua tarefa"
               {...register('description')}
             />
           </div>
-
-          <button onClick={() => setIsVisible(false)}>Cancelar</button>
-          <button onClick={handleSubmit(handleCreateTask)}>Criar</button>
+  
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+            onClick={() => setIsVisible(false)}
+          >
+            Cancelar
+          </button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+            onClick={handleSubmit(handleCreateTask)}
+          >
+            Criar
+          </button>
         </div>
       )}
-
+  
       {!isLoading && (
-        <table>
+        <table className="w-full mt-4">
           <thead>
-            <tr>
-              <th>ID</th>
-              <th>Título</th>
-              <th>Descrição</th>
+            <tr className="bg-gray-200">
+              <th className="p-2 bg-">ID</th>
+              <th className="p-2">Título</th>
+              <th className="p-2">Descrição</th>
+              <th className="p-2"></th>
             </tr>
           </thead>
           <tbody>
             {tasks.map((task) => {
               return (
-                <tr key={task.id}>
-                  <td>{task.id}</td>
-                  <td>{task.title}</td>
-                  <td>{task.description}</td>
+                <tr key={task.id} className="border-b">
+                  <td className="p-2">{task.id}</td>
+                  <td className="p-2">{task.title}</td>
+                  <td className="p-2">{task.description}</td>
+                  <td className='p-2'>
+                    <Trash onClick={() => handleDeleteTask(task.id)} />
+                  </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       )}
-
-      {isLoading && <div>Carregando...</div>}
+  
+      {isLoading && (
+        <div className="mt-4 text-gray-600">Carregando...</div>
+      )}
     </div>
-  )
+  );
+  
 }
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.15.186:8080/api/tasks',
+  baseURL: 'http://10.30.30.21:8080/api/tasks',
 })
